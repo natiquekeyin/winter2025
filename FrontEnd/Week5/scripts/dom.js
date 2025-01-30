75b74431baf034562cf65b8f0d58cc990930c7ad
Noman Atique <noman.atique@keyin.com>
2025-01-30 11:20:31 -0500

    Jan30

--- FrontEnd/Week5/scripts/dom.js
+++ FrontEnd/Week5/scripts/dom.js
@@ -14,9 +14,70 @@ window.addEventListener("DOMContentLoaded", function () {
   });
 
   let cat = this.document.querySelector("#cat");
+
+  let currentCatIndex = 0;
+  function nextImageUrl() {
+    currentCatIndex++;
+    if (currentCatIndex === cats.length - 1) {
+      currentCatIndex = 0;
+    }
+
+    return cats[currentCatIndex];
+  }
   cat.addEventListener("click", function (evt) {
-    console.log("cat clicked");
-    console.log(evt);
+    // console.log("cat clicked");
+    // console.log(evt);
+    cat.src = nextImageUrl();
+
     evt.stopPropagation();
   });
+
+  let btn1 = this.document.querySelector("#btn1");
+
+  btn1.addEventListener("click", function () {
+    btn1.remove();
+    cat.remove();
+
+    let div = document.querySelector("#list");
+    /* 
+    <div id = "list">
+    <ol>
+    <li>url...cats file </li>
+    ...
+
+    ..
+    </ol>
+    </div>
+    */
+
+    let ol = document.createElement("ol");
+
+    /* 
+<li>url..,/li>
+<li>...</ul>
+
+    */
+    let i = 0;
+    cats.forEach(function (url) {
+      let li = document.createElement("li");
+      let a = document.createElement("a");
+      a.href = url;
+      a.target = "_blank";
+      a.innerHTML = `Image- 0${i++}`;
+      li.appendChild(a);
+
+      ol.appendChild(li);
+    });
+    div.appendChild(ol);
+  });
+  // Timer functions...
+  this.setInterval(function () {
+    cat.src = nextImageUrl();
+  }, 3000);
+
+  this.setTimeout(function () {
+    alert("5 seconds gone");
+  }, 5000);
 });
+
+// these urls should be linked to themselves like when I click on them they should open the respective picture in a new tab....
