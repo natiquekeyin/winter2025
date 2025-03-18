4cc351f8fd0ee4d23b45387eedb8fe3046c33865
Noman Atique <noman.atique@keyin.com>
2025-03-18 11:27:00 -0400

    march18

--- JavaScript/Week11/tasktracker/src/App.jsx
+++ JavaScript/Week11/tasktracker/src/App.jsx
@@ -3,6 +3,11 @@ import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import { useState, useEffect } from "react";
 import AddTask from "./components/AddTask";
+import Footer from "./components/Footer";
+import About from "./components/About";
+import UState from "./components/topics/UState";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   const [tasks, setTasks] = useState([]);
@@ -28,6 +33,14 @@ function App() {
     return data;
   };
 
+  // Fetch the task
+  const fetchTask = async (id) => {
+    const res = await fetch(`http://localhost:5000/tasks/${id}`);
+
+    const data = await res.json();
+    return data;
+  };
+
   // Delete a task
 
   const deleteTask = async (id) => {
@@ -42,7 +55,18 @@ function App() {
 
   // Toggle reminder
 
-  const toggleReminder = (id) => {
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchTask(id);
+    const updTask = { ...taskToToggle, reminder: !taskToToggle.reminder };
+
+    const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(updTask),
+    });
+
     setTasks(
       tasks.map((task) =>
         task.id === id ? { ...task, reminder: !task.reminder } : task
@@ -52,34 +76,57 @@ function App() {
 
   // To add a new task
 
-  const addTask = (task) => {
+  const addTask = async (task) => {
     //  produce and id..
-    const id = Math.floor(Math.random() * 1000) + 1;
+    // const id = Math.floor(Math.random() * 1000) + 1;
     // let id = tasks.length + 1;
-    const newTask = { id, ...task };
+    const res = await fetch("http://localhost:5000/tasks", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(task),
+    });
 
-    setTasks([...tasks, newTask]);
+    const data = await res.json();
+
+    // const newTask = { id, ...task };
+
+    setTasks([...tasks, data]);
   };
   return (
-    <>
+    <Router>
       <div className="container">
         <Header
           title="Task Tracker"
           onAdd={() => setShowAddTask(!showAddTask)}
           showAdd={showAddTask}
         />
-        {showAddTask && <AddTask onAdd={addTask} />}
-        {tasks.length > 0 ? (
-          <Tasks
-            tasks={tasks}
-            onDelete={deleteTask}
-            onToggle={toggleReminder}
+
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                {showAddTask && <AddTask onAdd={addTask} />}
+                {tasks.length > 0 ? (
+                  <Tasks
+                    tasks={tasks}
+                    onDelete={deleteTask}
+                    onToggle={toggleReminder}
+                  />
+                ) : (
+                  "No tasks to display"
+                )}
+              </>
+            }
           />
-        ) : (
-          "No tasks to display"
-        )}
+          <Route path="/about" element={<About />} />
+          <Route path="/topics" element={<UState />} />
+        </Routes>
+        <Footer />
       </div>
-    </>
+    </Router>
   );
 }
 
---
+++ JavaScript/Week11/tasktracker/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Copyright &copy; 2025</p>
+      {/* <a href="/about">About</a> */}
+      <Link to="/about">About</Link>
+    </footer>
+  );
+};
+export default Footer;
