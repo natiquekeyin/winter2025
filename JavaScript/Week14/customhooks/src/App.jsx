7dde82de42dec4eee669fa98855c5284b57684f8
Noman Atique <noman.atique@keyin.com>
2025-04-02 08:22:57 -0400

    April2

--- JavaScript/Week14/customhooks/src/App.jsx
+++ JavaScript/Week14/customhooks/src/App.jsx
@@ -2,32 +2,43 @@ import "./App.css";
 import useDocTitle from "./hooks/useDocTitle";
 import useSmoothScroll from "./hooks/useSmoothScroll";
 import useLocalStorage from "./hooks/useLocalStorage";
+import useFetch from "./hooks/useFetch";
 
 function App() {
   const [docTitle, setDocTitle] = useDocTitle({ title: "Start Page" });
   const [refToScroll, smoothScroll] = useSmoothScroll();
   const [topRef, smoothScrollTop] = useSmoothScroll();
   const [count, SetCount] = useLocalStorage("count1");
+  const { data, loading, error } = useFetch("https://reqres.in/api/users");
   const increment = () => {
     SetCount(count + 1);
   };
-  return (
-    <>
-      <h1>Custom Hooks</h1>
-      <button onClick={() => setDocTitle("My new Page")}>Change Title</button>
-      <button onClick={smoothScroll} ref={topRef}>
-        Scroll
-      </button>
-      <p>Count:{count}</p>
-      <button onClick={increment}>Increment</button>
+  // return (
+  //   <>
+  //     <h1>Custom Hooks</h1>
+  //     {/* <button onClick={() => setDocTitle("My new Page")}>Change Title</button>
+  //     <button onClick={smoothScroll} ref={topRef}>
+  //       Scroll
+  //     </button>
+  //     <p>Count:{count}</p>
+  //     <button onClick={increment}>Increment</button>
 
-      <div style={{ marginTop: "150vh" }} ref={refToScroll}>
-        {" "}
-        I am at bottom of the screen
-      </div>
-      <button onClick={smoothScrollTop}>Go To Top!</button>
-    </>
-  );
+  //     <div style={{ marginTop: "150vh" }} ref={refToScroll}>
+  //       {" "}
+  //       I am at bottom of the screen
+  //     </div>
+  //     <button onClick={smoothScrollTop}>Go To Top!</button> */}
+  //     {loading ? <h4>Loading....</h4> : <div>{data.data[0].emai}</div>}
+  //   </>
+  // );
+
+  if (loading) {
+    return <div>loading...</div>;
+  }
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+  return <div>{data.data[0].email}</div>;
 }
 
 export default App;
---
+++ JavaScript/Week14/customhooks/src/hooks/useFetch.jsx
@@ -0,0 +1,27 @@
+// fetch the data for a source and so that i can use here...
+
+import { useEffect, useState } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        const json = await response.json();
+        setData(json);
+        setLoading(false);
+      } catch (error) {
+        setError(error);
+        setLoading(false);
+      }
+    };
+    fetchData();
+  }, [url]);
+
+  return { data, loading, error };
+};
+export default useFetch;
